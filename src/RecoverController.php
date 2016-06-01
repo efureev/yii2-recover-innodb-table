f8c70f3a152b842fffbf8ca4bf4b8ac6a672a520
efureev <efureev@yandex.ru>
2016-06-02 04:15:28 +0500

    fix

--- src/RecoverController.php
+++ src/RecoverController.php
@@ -15,8 +15,6 @@ class RecoverController extends Controller
 
     public function actionRepairTable($pathDb, $pathDbBak, $mode = 0660)
     {
-
         DbUtils::repairTable($pathDb, $pathDbBak, $mode, $this->excludeTables);
-//        $tools->repairTable('/usr/local/var/mysql/miladoma_auth', '/usr/local/var/mysql/miladoma_auth_bak', $excludeTables);
     }
 }
